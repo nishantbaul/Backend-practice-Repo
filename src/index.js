50315d882d1b0a391a752859423205b912972b85
Nishant Baul <nishantbaul@gmail.com>
2025-12-11 18:40:19 +0530

    some

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
 //connection of batabase!
 require('dotenv').config({path:'./env'}) 
 
-//2nd method of connect databas
- /*
-import dotenv from "dotenv"
-import connectDB from "./db/index.js";
-import {app} from './app.js'
-
-dotenv.config({
-    path:'./env'
-})
-
-connectDB
-.then(() =>{
-    app.listen(process.env.PORT ||3000,() =>{
-        console.log(`Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) =>{
-    console.log("MONGO db connection failed !",err);
-}) 
-    */
-
 
 //1st method to connect database
  
@@ -49,4 +28,27 @@ const app = express();
         throw error;
     }
 })();
+
+//2nd method of connect databas
+ /*
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB
+.then(() =>{
+    app.listen(process.env.PORT ||3000,() =>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) =>{
+    console.log("MONGO db connection failed !",err);
+}) 
+    */
+
+
 
