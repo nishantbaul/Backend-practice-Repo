621cc8e351636fdba7979d7173285ee21ce5841e
Nishant Baul <nishantbaul@gmail.com>
2025-12-12 20:01:37 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {ApiError} from "..//utils//ApiError.js" 
+import {ApiError} from "../utils/ApiError.js" 
 import {User} from "../models//user.model.js"
-import {uoloadOnClousinary} from "../utils//cloudinary.js"
+import {uploadOnClousinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"; 
 
 const registerUser = asyncHandler(async (req, res) => {
@@ -17,29 +17,36 @@ const registerUser = asyncHandler(async (req, res) => {
     */
 
     const {fullName, email, username, password} = req.body
-    console.log("email:", email );
+   //console.log("email:", email );
 
     if (  [
        fullName, email, username , password
     ].some 
-    (    (field)  => field ?.  trim() == "")   ) 
+    (    (field)  => field ?.  trim() === "")   ) 
     {
         throw new ApiError(400,"all field are required")
     } 
-   const existedUser = User.findOne({
+   const existedUser = await User.findOne({
         $or:[{username}, {email}]
     })
     if (existedUser) {
         throw new ApiError(409,"User with email or username already exists")
 
     }
+
+    console.log(req.files );
      const avatarLocalPath = req.files?.avatar[0]?.path;
-     const coverImageLocalPath = req.files?.coverImage[0]?.path;
+     // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+     let coverImageLocalPath;
+     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+     }
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar fiel is required")
     }
-    const avatar = await uoloadOnClousinary(avatarLocalPath)
-    const coverImage = await uoloadOnClousinary(coverImageLocalPath)
+    const avatar = await uploadOnClousinary(avatarLocalPath)
+    const coverImage = await uploadOnClousinary(coverImageLocalPath)
 
     if (!avatar) {
         throw new ApiError(400, "Avatar fiel is required")
--- src/index.js
+++ src/index.js
@@ -1,54 +1,46 @@
-//connection of batabase!
-require('dotenv').config({path:'./env'}) 
-
-
-//1st method to connect database
- 
- import mongoose from "mongoose";
-import { DB_NAME } from "./constant.js";
-import express from "express";
-
-const app = express();
-
-(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-
-        app.on("error", (error) => {
-            console.log("ERROR:", error);
-            throw error;
-        });
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        });
-
-    } catch (error) {
-        console.error("ERROR:", error);
-        throw error;
-    }
-})();
-
-//2nd method of connect databas
- /*
+// require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import {app} from './app.js'
-
 dotenv.config({
-    path:'./env'
+    path: './.env'
 })
 
-connectDB
-.then(() =>{
-    app.listen(process.env.PORT ||3000,() =>{
-        console.log(`Server is running at port : ${process.env.PORT}`);
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
-.catch((err) =>{
-    console.log("MONGO db connection failed !",err);
-}) 
-    */
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
